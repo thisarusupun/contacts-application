e54ad628e3d659a523fd0563764e2e45b7a6b46e
Thisaru Supun <thisarusupun1314@gmail.com>
2024-05-31 18:36:11 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(
     origin: ["https://contacts-application-client.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use("/api/contacts", contactRoute);
